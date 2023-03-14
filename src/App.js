0fc64bab7a35e2f8ebeaeb2363b36c476a307caa
Nigel Sheridan-Smith <nigel@greenshoresdigital.com>
2023-03-15 08:21:41 +1100

    Display the results

--- src/App.js
+++ src/App.js
@@ -3,34 +3,49 @@ import './App.css';
 import { firebaseApp } from "./firebase-setup";
 import { getFirestore } from "firebase/firestore";
 import { collection, addDoc, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(firebaseApp);
 
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
+// try {
+//   const docRef = await addDoc(collection(db, "users"), {
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815
+//   });
+//   console.log("Document written with ID: ", docRef.id);
+// } catch (e) {
+//   console.error("Error adding document: ", e);
+// }
+
+async function getData() {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  console.log('querySnapshot', querySnapshot)
+  var results = new Map()
+  querySnapshot.forEach((doc) => results.set(doc.id, doc.data()))
+  console.log('results', results)
+  return results
 }
 
-// const querySnapshot = await getDocs(collection(db, "users"));
-// querySnapshot.forEach((doc) => {
-//   console.log(`${doc.id} => ${doc.data()}`);
-// });
-
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData().then((result) => setData(result))
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          { Array.from(data.entries()).map((entry) => {
+              const [key, value] = entry;
+              return (<li key={key}>{value.first} - {value.last}</li>);
+            })
+          }
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
